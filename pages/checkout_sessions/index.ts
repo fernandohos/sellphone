9c67d277f28c4f48bfd88536238f27de0805c375
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-02 18:55:29 -0300

    :building_construction: create checkout session with stripe

---
+++ pages/checkout_sessions/index.ts
@@ -0,0 +1,31 @@
+import Stripe from "stripe";
+import { NextApiHandler } from 'next';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+    apiVersion: "2020-08-27"
+});
+
+const handler: NextApiHandler = (req, res) => {
+    if (req.method === 'POST') {
+        try {
+            const session = stripe.checkout.sessions.create({
+                mode: "payment",
+                cancel_url: req.headers.origin + "/cart",
+                success_url: req.headers.origin + "/success?session_id={CHECKOUT_SESSION_ID}",
+                payment_method_types: ["card"],
+                line_items: req.body.items ?? []
+            })
+
+            res.status(200).json(session);
+        }
+        catch ({ message }) {
+            res.status(500).json({ statusCode: 500, message });
+        }
+    }
+    else {
+        res.setHeader("Allow", "POST");
+        res.status(405).end("Method Not Allowed");
+    }
+}
+
+export default handler;
