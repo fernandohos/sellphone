ce1de9fe4e89639828e19b496a627cd0da759b5c
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-02 21:34:28 -0300

    :building_construction: get info by session id endpoint

---
+++ pages/checkout_sessions/[id].ts
@@ -0,0 +1,27 @@
+import { NextApiHandler } from "next";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+    apiVersion: "2020-08-27"
+});
+
+const handler: NextApiHandler = (req, res) => {
+    let { query: { id } } = req;
+
+    try {
+        if (typeof id !== 'string') {
+            id = id[0];
+        }
+
+        if (!id.startsWith('cs_')) {
+            throw Error("Incorrect Checkout Session ID");
+        }
+        const checkout_session = stripe.checkout.sessions.retrieve(id);
+        res.status(200).json(checkout_session);
+    }
+    catch ({ message }) {
+        res.status(500).json({ statusCode: 500, message });
+    }
+}
+
+export default handler;
