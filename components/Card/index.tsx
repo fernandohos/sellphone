4ef04d8427705753949c2cdb0552d6a75be9cac2
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-05 21:50:04 -0300

    :construction: card component

---
+++ components/Card/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as C from './styles';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type Product = {
+    id: string;
+    name: string;
+    images: string[];
+    description: string;
+    price: number;
+}
+
+type CardProps = {
+    item: Product;
+}
+
+export function Card({ item }: CardProps) {
+    return (
+        <Link href={"/product/" + item.id}>
+            <a style={{ textDecoration: "none" }}>
+                <C.Card>
+                    <div className="image-container">
+                        <Image
+                            src={item.images[0]}
+                            alt={item.name}
+                            width={400}
+                            height={400}
+                            objectFit="contain"
+                        />
+                    </div>
+                    <p title={item.name}>{item.name.slice(0, 50)}{item.name.length > 50 ? '...' : ''}</p>
+                    <div className="flex">
+                        <p>R${item.price}23334,00</p>
+                        <C.Button>
+                            <p>add to bag</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M19 6h-2c0-2.76-2.24-5-5-5S7 3.24 7 6H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7-3c1.66 0 3 1.34 3 3H9c0-1.66 1.34-3 3-3zm7 17H5V8h14v12zm-7-8c-1.66 0-3-1.34-3-3H7c0 2.76 2.24 5 5 5s5-2.24 5-5h-2c0 1.66-1.34 3-3 3z" /></svg>
+                        </C.Button>
+                    </div>
+                </C.Card>
+            </a>
+        </Link>
+    )
+}
---
+++ components/Card/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: .3rem;
+    display: flex; 
+    flex-direction: column;
+    border: 1px solid var(--primary);
+    text-decoration: none;
+
+    .flex {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: .5rem;
+
+        > p {
+            color: var(--text);
+            font-weight: 600;
+            font-size: 2.2rem;
+        }
+    }
+
+    .image-container {
+        background: #fff;
+        width: 100%;
+        height: auto;
+        padding: 1rem;
+    }
+
+    > p {
+        color: var(--text);
+        padding: .5rem;
+        width: 100%;
+        font-size: 1.8rem;
+    }
+`;
+
+export const Button = styled.button`
+    padding: 1rem;
+    background: var(--btn);
+    color: #fff;
+    display: flex;
+    border: none;
+    border-radius: .3rem;
+    align-items: center;
+    align-self: flex-end;
+    width: max-content;
+    gap: .5rem;
+    font-size: 1.8rem;
+
+    svg {
+        width: 2rem;
+        height: 2rem;
+        fill: #fff;
+    }
+`;
