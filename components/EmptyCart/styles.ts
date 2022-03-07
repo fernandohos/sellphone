bb87b8d630346565942a0b337d5660535fff38b5
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-03-06 22:34:05 -0300

    :construction: empty cart component

---
+++ components/EmptyCart/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const EmptyCart = styled.main`
+    margin-top: 17vh;
+
+    > p {
+        font-size: 2rem;
+        color: var(--text);
+    }
+
+`;
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -3,14 +3,24 @@ import { CheckoutButton } from '@components/CheckoutButton';
 import { Header } from '@components/Header';
 import { CartItems } from '@components/CartItems';
 import * as C from '@styles/cart';
+import { useShoppingCart } from '@hooks/useShoppingCart';
+import { EmptyCart } from '@components/EmptyCart';
 
 export default function Cart() {
+    const { cartItems } = useShoppingCart();
     return (
         <>
             <Header />
             <C.Cart>
-                <CartItems />
-                <CheckoutButton />
+                {
+                    cartItems.length ?
+                        <>
+                            <CartItems />
+                            <CheckoutButton />
+                        </>
+                        :
+                        <EmptyCart />
+                }
             </C.Cart>
         </>
     )
